90ad3ff992f7be78281800793e77d67d5c910c7f
XiaxiaTnT <Ray_xyxia@163.com>
2017-04-30 22:48:18 +0800

    2017/4/30

--- Demo1/app/src/main/java/com/example/xiaxia/demo1/MainActivity.java
+++ Demo1/app/src/main/java/com/example/xiaxia/demo1/MainActivity.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.os.Handler;
 import android.os.Looper;
+import android.os.Message;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,6 +25,8 @@ import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 import java.net.URL;
+import java.util.logging.LogRecord;
+
 public class MainActivity extends AppCompatActivity {
     Button Test,Postion;
     ArrayList<String> onlineaps;
@@ -37,6 +42,17 @@ public class MainActivity extends AppCompatActivity {
 
         Test=(Button)findViewById(R.id.button_test);
         Postion=(Button)findViewById(R.id.button_pos);
+        final Handler handler=new Handler() {
+            @Override
+            public void handleMessage(Message msg){
+                switch (msg.what){
+                    case 3:
+                        new AlertDialog.Builder(MainActivity.this).setTitle("结果").setItems(new String[]{msg.obj.toString()},null).setNegativeButton("确定",null).show();
+                }
+            }
+
+        };
+        wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         Test.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
@@ -56,24 +72,32 @@ public class MainActivity extends AppCompatActivity {
                         sumlist=new ArrayList<>();
                         timeslist=new ArrayList<>();
                         String res="";
-                        wifiManager=(WifiManager)getSystemService(Context.WIFI_SERVICE);
+
                         if(!wifiManager.isWifiEnabled()){
                             wifiManager.setWifiEnabled(true);
                         }
-                        for(int i=0;i<64;i++) {
-                            list = wifiManager.getScanResults();
-                            for(int j=0;j<list.size();j++){
-                                ScanResult scanResult=list.get(j);
-                                if(onlineaps.contains(scanResult.BSSID)){
-                                    sumlist.set(onlineaps.indexOf(scanResult.BSSID),sumlist.get(onlineaps.indexOf(scanResult.BSSID))+Double.valueOf(scanResult.level));
-                                    timeslist.set(onlineaps.indexOf(scanResult.BSSID),timeslist.get(onlineaps.indexOf(scanResult.BSSID))+1);
-                                }else if(!onlineaps.contains(scanResult.BSSID)){
-                                    onlineaps.add(scanResult.BSSID);
-                                    sumlist.add(Double.valueOf(scanResult.level));
-                                    timeslist.add(1);
+                        try {
+                            for (int i = 0; i < 64; i++) {
+                               // wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+                                wifiManager.startScan();
+                                list = wifiManager.getScanResults();
+                                for (int j = 0; j < list.size(); j++) {
+                                    ScanResult scanResult = list.get(j);
+                                    if (onlineaps.contains(scanResult.BSSID)) {
+                                        sumlist.set(onlineaps.indexOf(scanResult.BSSID), sumlist.get(onlineaps.indexOf(scanResult.BSSID)) + Double.valueOf(scanResult.level));
+                                        timeslist.set(onlineaps.indexOf(scanResult.BSSID), timeslist.get(onlineaps.indexOf(scanResult.BSSID)) + 1);
+                                    } else if (!onlineaps.contains(scanResult.BSSID)) {
+                                        onlineaps.add(scanResult.BSSID);
+                                        sumlist.add(Double.valueOf(scanResult.level));
+                                        timeslist.add(1);
+                                    }
                                 }
+                                Thread.sleep(200);
                             }
+                        }catch (InterruptedException e){
+                            e.printStackTrace();
                         }
+                        Toast.makeText(getApplicationContext(),"采集信息完成",Toast.LENGTH_LONG).show();
                         for (int i=0;i<onlineaps.size();i++){
                             res+=onlineaps.get(i);
                             res+="=";
@@ -97,7 +121,12 @@ public class MainActivity extends AppCompatActivity {
                                 String line;
                                 StringBuilder s = new StringBuilder();
                                 while ((line = reader.readLine()) != null) {
-                                    Toast.makeText(getApplicationContext(),line,Toast.LENGTH_LONG).show();
+                                    Message message=new Message();
+                                    message.what=3;
+                                    message.obj=line;
+                                    handler.sendMessage(message);
+                                    //Toast.makeText(getApplicationContext(),line,Toast.LENGTH_LONG).show();
+
                                     s.append(line);
                                 }
                                 reader.close();
--- Demo1/app/src/main/java/com/example/xiaxia/demo1/TestActivity.java
+++ Demo1/app/src/main/java/com/example/xiaxia/demo1/TestActivity.java
@@ -1,16 +1,24 @@
 package com.example.xiaxia.demo1;
 
 import android.app.Activity;
+import android.app.Notification;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -23,28 +31,81 @@ import java.net.URL;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
 
 /**
  * Created by Xiaxia on 2017/4/5.
  */
 
 public class TestActivity extends Activity {
-    EditText editposx,editposy;
+//    EditText editposx,editposy;
     TextView textviewposx,textviewposy;
     Button btn_one,btn_done;
     ArrayList<String> aps;
     ArrayList<Point> points;
+    ArrayList<String> postion;
     List<ScanResult> list;
+    NumberPicker numberPicker1,numberPicker2;
     private WifiManager wifiManager;
+    ListView listView,listView1;
+    ArrayAdapter<String> adapter;
+    ArrayAdapter<String> adapter1;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
         aps=new ArrayList<>();
+        postion=new ArrayList<>();
+
+
+        listView=(ListView)findViewById(R.id.listview) ;
+        adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,aps);
+        listView.setAdapter(adapter);
+
+        listView1=(ListView)findViewById(R.id.listview1);
+        adapter1=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,postion);
+        listView1.setAdapter(adapter1);
+
+        final Handler handler=new Handler() {
+            @Override
+            public void handleMessage(Message msg){
+                switch (msg.what){
+                    case 4:
+                        adapter.notifyDataSetChanged();
+                        break;
+                    case 5:
+                        Toast.makeText(TestActivity.this,"离线采集一次完成",Toast.LENGTH_LONG).show();
+                        break;
+                    case 6:
+                        adapter1.notifyDataSetChanged();
+                        break;
+                }
+
+            }
+        };
+
+        String[] values=new String[]{
+               "0.0","0.5","1.0","1.5","2.0","2.5","3.0","3.5","4.0","4.5","5.0","5.5","6.0","6.5","7.0","7.5","8.0","8.5","9.0","9.5",
+                "10.0","10.5","11.0","11.5","12.0","12.5","13.0","13.5","14.0","14.5","15.0","15.5","16.0","16.5","17.0","17.5","18.0","18.5","19.0","19.5",
+                "20.0","20.5","21.0","21.5","22.0","22.5","23.0","23.5","24.0","24.5","25.0","25.5","26.0","26.5","27.0","27.5","28.0","28.5","29.0","29.5",
+        };
+        numberPicker1=(NumberPicker)findViewById(R.id.np1);
+        numberPicker2=(NumberPicker)findViewById(R.id.np2);
+        numberPicker1.setMinValue(0);
+        numberPicker1.setMaxValue(values.length-1);
+        numberPicker1.setDisplayedValues(values);
+        numberPicker2.setMinValue(0);
+        numberPicker2.setMaxValue(values.length-1);
+        numberPicker2.setDisplayedValues(values);
+
         points=new ArrayList<>();
         wifiManager=(WifiManager)getSystemService(Context.WIFI_SERVICE);
-        editposx=(EditText)findViewById(R.id.pos_x);
-        editposy=(EditText)findViewById(R.id.pos_y);
+//        editposx=(EditText)findViewById(R.id.pos_x);
+//        editposy=(EditText)findViewById(R.id.pos_y);
         textviewposx=(TextView)findViewById(R.id.posx);
         textviewposy=(TextView)findViewById(R.id.posy);
         btn_one=(Button)findViewById(R.id.getone);
@@ -53,8 +114,10 @@ public class TestActivity extends Activity {
             @Override
             public void onClick(View v) {
                 new Thread(new Runnable() {
+
                     @Override
                     public void run() {
+                        Looper.prepare();
                         String doaps="insert into aps values ";
                         for(int i=0;i<aps.size();i++){
                             String ss="('"+aps.get(i)+"')";
@@ -110,27 +173,55 @@ public class TestActivity extends Activity {
                     @Override
                     public void run() {
                         //获取点位置信息
-                        Point p=new Point(Double.parseDouble(editposx.getText().toString()),Double.parseDouble(editposy.getText().toString()));
-                        if(!points.contains(p)){
-                            points.add(p);
-                        }
-                        if(!wifiManager.isWifiEnabled()){
-                            wifiManager.setWifiEnabled(true);
-                        }
-                        String rssvalue="insert into Rss values ";
-                        for(int i=0;i<64;i++) {
-                            String s="('";
-                            s=s+p.getX()+"','"+p.getY()+"','";
-                            list = wifiManager.getScanResults();
-                            for(int j=0;j<list.size();j++){
-                                ScanResult scanResult=list.get(j);
-                                if(!aps.contains(scanResult.BSSID)){
-                                    aps.add(scanResult.BSSID);
-                                }
-                                s=s+scanResult.BSSID+"="+scanResult.level+";";
+                        //Looper.prepare();
+                        String rssvalue = "insert into Rss values ";
+                        try {
+                            //wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+                            //Point p = new Point(Double.parseDouble(editposx.getText().toString()), Double.parseDouble(editposy.getText().toString()));
+                            Point p = new Point(numberPicker1.getValue()*0.5,numberPicker2.getValue()*0.5);
+                            if (!points.contains(p)) {
+                                points.add(p);
+                                String s_p="("+p.getX()+","+p.getY()+")";
+                                postion.add(s_p);
                             }
-                            s=s+"')";
-                            rssvalue=rssvalue+s+(i==63?';':',');
+                            if (!wifiManager.isWifiEnabled()) {
+                                wifiManager.setWifiEnabled(true);
+                            }
+                            //String rssvalue = "insert into Rss values ";
+                            for (int i = 0; i < 64; i++) {
+                                String s = "('";
+                                s = s + p.getX() + "','" + p.getY() + "','";
+                               // wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+                                wifiManager.startScan();
+                                list = wifiManager.getScanResults();
+                                for (int j = 0; j < list.size(); j++) {
+                                    ScanResult scanResult = list.get(j);
+                                    if (!aps.contains(scanResult.BSSID)) {
+                                        aps.add(scanResult.BSSID);
+//                                        Message message=new Message();
+//                                        message.what=4;
+//                                        handler.sendMessage(message);
+                                    }
+                                    s = s + scanResult.BSSID + "=" + scanResult.level + ";";
+                                }
+                                s = s + "')";
+                                rssvalue = rssvalue + s + (i == 63 ? ';' : ',');
+                                Thread.sleep(200);
+                            }
+                            //Toast.makeText(TestActivity.this,"离线采集一次完成",Toast.LENGTH_LONG).show();
+                            Message message5=new Message();
+                            message5.what=5;
+                            handler.sendMessage(message5);
+                            //aps
+                            Message message4=new Message();
+                            message4.what=4;
+                            handler.sendMessage(message4);
+                            //postion
+                            Message message6=new Message();
+                            message6.what=6;
+                            handler.sendMessage(message6);
+                        }catch (InterruptedException e){
+                            e.printStackTrace();
                         }
 
                         try{
@@ -162,6 +253,7 @@ public class TestActivity extends Activity {
                         }
                     }
                 }).start();
+
             }
         });
     }
